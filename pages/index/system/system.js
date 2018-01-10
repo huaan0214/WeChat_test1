f4650fb0a03d84639db03299a0e48ccfdc419fb1
huaan0214 <1097634923@qq.com>
2018-01-10 23:39:42 +0800

    update-0110-2339

--- pages/index/system/system.js
+++ pages/index/system/system.js
@@ -1,4 +1,5 @@
 // pages/index/system/system.js
+var positionZ=[0,0,0,0,0]
 Page({
 
   /**
@@ -20,7 +21,16 @@ Page({
     getInfoReady:0,
     getNetworkReady: 0,
     networkType: "",
-    isConnected:""
+    isConnected:"",
+    x:0,
+    y:0,
+    z:0,
+
+    phoneNumber:"18549811330",
+    scanCodeResult:"无",
+    scanCodePath:"无",
+
+    nowDirection:""
   
   },
   getSysInfo:function(){
@@ -51,6 +61,26 @@ Page({
       getNetworkReady:0  
     })
   },
+callPhone:function(){
+  wx.makePhoneCall({
+    phoneNumber: this.data.phoneNumber,
+  })
+},
+scanCodes:function(){
+  wx.scanCode({
+    onlyFromCamera:false,
+    success:res=>{
+      this.setData({
+      scanCodeResult:res.result
+      })
+      if(res.path)
+      this.setData({
+        scanCodePath:res.path
+      })
+      
+    }
+  })
+},
   aboutCanIUse:function(){
     wx.canIUse('openBluetoothAdapter')
     wx.canIUse('getSystemInfoSync.return.screenWidth')
@@ -90,9 +120,60 @@ Page({
           isConnected: "网络未连接",
           getNetworkReady: 1
         })
-
     })
+  wx.onAccelerometerChange(res=>{
+    var Z=res.z
+    var Y=res.y
+    var X=res.x
+    var sumZ=0
+    var aveZ=0
+    this.setData({
+      x:X,
+      y:Y,
+      z:Z
+    })
+    for(var i=0;i<(positionZ.length-1);i++)
+    positionZ[i]=positionZ[i+1]
+    positionZ[positionZ.length-1]=Z
+    for (var i = 0; i < positionZ.length ; i++)
+    sumZ+=positionZ[i]
+    aveZ = sumZ/positionZ.length
   
+    if(aveZ>-0.82)
+    {
+      wx.getSystemInfo({
+        success: res => {
+          this.setData({
+            model: res.model,
+            pixelRatio: res.pixelRatio,
+            screenWidth: res.screenWidth,
+            screenHeight: res.screenHeight,
+            windowWidth: res.windowWidth,
+            windowHeight: res.windowHeight,
+            language: res.language,
+            version: res.version,
+            system: res.system,
+            platform: res.platform,
+            SDKVersion: res.SDKVersion,
+
+            getInfoReady: 1
+          })
+        }
+      })
+    }
+      else 
+      this.setData({
+        getInfoReady: 0,
+        getNetworkReady: 0
+      }) 
+   
+  })
+
+  wx.onCompassChange(res=>{
+    this.setData({
+      nowDirection:res.direction
+    })
+  })
   },
 
   /**
